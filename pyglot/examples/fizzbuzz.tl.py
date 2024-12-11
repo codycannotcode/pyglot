f8330bf7d78f957cefd3e7a8f42738a51363b43c
codycannotcode <49048219+codycannotcode@users.noreply.github.com>
2024-12-11 14:25:26 -0800

    more examples

---
+++ pyglot/examples/fizzbuzz.tl.py
@@ -0,0 +1,9 @@
+para i sa saklaw(1, 20):
+    kung i % 5 == 0 at i % 3 == 0:
+        iprinta('FizzBuzz')
+    kung_kasunod i % 3 == 0:
+        iprinta('Fizz')
+    kung_kasunod i % 5 == 0:
+        iprinta('Buzz')
+    kung_hindi:
+        iprinta(i)
---
+++ pyglot/examples/fizzbuzz.vi.py
@@ -0,0 +1,9 @@
+if i trong phạm_vi(1, 20):
+    nếu i % 5 == 0 và i % 3 == 0:
+        in('FizzBuzz')
+    nếu_không i % 3 == 0:
+        in('Fizz')
+    nếu_không i % 5 == 0:
+        in('Buzz')
+    khác:
+        in(i)
