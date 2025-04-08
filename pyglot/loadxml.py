8694eedb3e0d7f217988e6f2dc4bfc8aa7d0b930
codycannotcode <49048219+codycannotcode@users.noreply.github.com>
2025-04-07 17:23:17 -0700

    implemented xsd validation into main pyglot program

--- pyglot/loadxml.py
+++ pyglot/loadxml.py
@@ -3,28 +3,28 @@ import xmlschema
 from os import path
 
 schema = xmlschema.XMLSchema(path.join('localizations', 'schema.xsd'))
+ns = {'': 'http://pyglot.com/schema'}
 
 def translations_from_xml(file_path):
     schema.validate(file_path)
 
-    root = ET.parse(file_path)
-    # keywords = tree.getroot().find('keywords')
-    # functions = tree.getroot().find('functions')
-    print(root.getroot().attrib)
+    tree = ET.parse(file_path)
+    keywords = tree.find('keywords', ns)
+    functions = tree.find('functions', ns)
 
     translation_map = {}
 
-    # for element in functions.findall('function'):
-    #     translation = element.text
-    #     native = element.get('name')
-    #     if translation and native:
-    #         translation_map[translation] = native
-    
-    # for element in keywords.findall('keywords'):
-    #     translation = element.text
-    #     native = element.get('key')
-    #     if translation and native:
-    #         translation_map[translation] = native
+    for element in functions.findall('function', ns):
+        translation = element.text
+        native = element.get('name')
+        if translation and native:
+            translation_map[translation] = native
+
+    for element in keywords.findall('keyword', ns):
+        translation = element.text
+        native = element.get('key')
+        if translation and native:
+            translation_map[translation] = native
 
     return translation_map
     
